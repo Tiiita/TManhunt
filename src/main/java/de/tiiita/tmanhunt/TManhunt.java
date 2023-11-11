41717a3fc8ce47c4ff38b6399144f0f0e3213c42
Tiiita <96084154+Tiiita@users.noreply.github.com>
2023-11-11 16:54:10 +0100

    Initial commit

---
+++ src/main/java/de/tiiita/tmanhunt/TManhunt.java
@@ -0,0 +1,17 @@
+package de.tiiita.tmanhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TManhunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
