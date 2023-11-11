ef628323f676e26771d255c545eaff3073cfa4f3
Tiiita <96084154+Tiiita@users.noreply.github.com>
2023-11-11 16:59:29 +0100

    Add base

--- src/main/java/de/tiiita/tmanhunt/TManhunt.java
+++ src/main/java/de/tiiita/tmanhunt/TManhunt.java
@@ -4,14 +4,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TManhunt extends JavaPlugin {
 
+
     @Override
     public void onEnable() {
         // Plugin startup logic
 
+        registerListener();
+        registerCommands();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+
+    private void registerCommands() {
+
+
+    }
+    private void registerListener() {
+
+
     }
 }
